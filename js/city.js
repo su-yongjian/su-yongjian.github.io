df55a6b678cb77acec121df0ce1161198a5ccecd
suyongjian <ada_yongjian@163.com>
2017-12-14 18:29:11 +0800

    0004版

---
+++ js/city.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Administrator on 2017/5/7.
+ */
+$(function () {
+    //加载城市事件
+    $('.container').show();
+//选择城市 start
+    $('body').on('click', '.city-list p', function () {
+        // alert($(this).html());
+        
+        window.location.href="../../index.html";
+        $(".location-s").text($(this).html());
+    });
+    //点击索引查询城市
+    $('body').on('click', '.letter a', function () {
+        var s = $(this).html(); // A  #A1
+        $(window).scrollTop( $('#' + s + '1').offset().top  - $("#header").height()-5);
+        $("#showLetter span").html(s);
+        $("#showLetter").show().delay(500).hide(0);
+    });
+     //中间的标记显示
+     $('body').on('onMouse', '.showLetter span', function () {
+        $("#showLetter").show().delay(500).hide(0);
+    });
+
+})
