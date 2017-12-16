0aec6122bb13c2736c14984f25e671839f619089
苏勇坚 <721765355@qq.com>
2017-12-16 18:48:58 +0800

    版本0005

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(function(){
+    $("#testQualificationBtn").on("click",function(){
+        window.location.href="../html/sonHTML/byStagesBuyCar.html";
+    });
+    $("#seeMoreCarBtn").on("click",function(){
+        window.location.href="../html/buyCar.html";
+    });
+})
