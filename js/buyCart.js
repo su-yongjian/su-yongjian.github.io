3b21f8dbc51a09039659537ced075eea5bc09ecd
suyongjian <ada_yongjian@163.com>
2017-12-18 13:10:13 +0800

    买车页切换功能实现

--- js/buyCart.js
+++ js/buyCart.js
@@ -1,44 +1,63 @@
-// $(function(){
+$(function(){
 
-//     $(".conditionScreen").on("click","li",function(){
-//         var $this = $(this);
-// // 点击当前li的时候，判断当前的是否有select-brand属性 ，  不限品牌div显示，遮罩层显示，其他不限项隐藏，同时当前li的兄弟元素的i标签的上标头都变为下标头。
-// // 遮罩层只要是有上标头都显示
-       
-//         // 不限品牌
-//         if($this.hasClass("select-brand")) {
-//             $(".unlimited-brand").toggle();
-//             $(".unlimited-price").hide();
-//             $(".unlimited-firstpay").hide();
-//             $(".unlimited-month").hide();
-//             if($(".unlimited-brand").is(":visible")){
-//                 $this.find("i").removeClass("mui-icon mui-icon-arrowdown").addClass("mui-icon mui-icon-arrowup")
-//             }else {
-//                 $this.find("i").removeClass("mui-icon mui-icon-arrowup").addClass("mui-icon mui-icon-arrowdown")
-//             }   
-            
-//             // 不限车价
-//         }else if($this.hasClass("select-price")) {
-//             $(".unlimited-price").toggle();
-//             $(".unlimited-brand").hide();
-//             $(".unlimited-firstpay").hide();
-//             $(".unlimited-month").hide();
+    $(".conditionScreen").on("click","li",function(){
+        var $this = $(this);
+        var flag = true ;
+        if($this.hasClass("select-brand")){
+            // 不限品牌选择box显示，其他隐藏
+            $(".unlimited-brand").toggle();
+            $(".unlimited-price").hide();
+            $(".unlimited-firstpay").hide();
+            $(".unlimited-month").hide();
+            // 上下箭头指向
+            if($(".unlimited-brand").is(":visible")){
+                $(".select-brand i").removeClass("mui-icon mui-icon-arrowdown").addClass("mui-icon mui-icon-arrowup");
+            }else {
+                $(".select-brand i").removeClass("mui-icon mui-icon-arrowup").addClass("mui-icon mui-icon-arrowdown");
+            }
+        }else if($this.hasClass("select-price")){
+            // 不限车价选择box显示，其他隐藏
+            $(".unlimited-brand").hide();
+            $(".unlimited-price").toggle();
+            $(".unlimited-firstpay").hide();
+            $(".unlimited-month").hide();
+            // 上下箭头指向
+            if($(".unlimited-price").is(":visible")){
+                $(".select-price i").removeClass("mui-icon mui-icon-arrowdown").addClass("mui-icon mui-icon-arrowup");
+            }else {
+                $(".select-price i").removeClass("mui-icon mui-icon-arrowup").addClass("mui-icon mui-icon-arrowdown");
+            }
+        } else if($this.hasClass("select-pay")){
+            // 不限首付选择box显示，其他隐藏
+            $(".unlimited-brand").hide();
+            $(".unlimited-price").hide();
+            $(".unlimited-firstpay").toggle();
+            $(".unlimited-month").hide();
+            // 上下箭头指向
+            if($(".unlimited-firstpay").is(":visible")){
+                $(".select-pay i").removeClass("mui-icon mui-icon-arrowdown").addClass("mui-icon mui-icon-arrowup");
+            }else {
+                $(".select-pay i").removeClass("mui-icon mui-icon-arrowup").addClass("mui-icon mui-icon-arrowdown");
+            }
+        } else if($this.hasClass("select-month")){
+            // 不限月供选择box显示，其他隐藏
+            $(".unlimited-brand").hide();
+            $(".unlimited-price").hide();
+            $(".unlimited-firstpay").hide();
+            $(".unlimited-month").toggle();
+            // 上下箭头指向
+            if($(".unlimited-month").is(":visible")){
+                $(".select-month i").removeClass("mui-icon mui-icon-arrowdown").addClass("mui-icon mui-icon-arrowup");
+            }else {
+                $(".select-month i").removeClass("mui-icon mui-icon-arrowup").addClass("mui-icon mui-icon-arrowdown");
+            }
+        } 
 
-//             // 不限首付
-//         }else if($this.hasClass("select-pay")) {
-//             $(".unlimited-firstpay").toggle();
-//             $(".unlimited-brand").hide();
-//             $(".unlimited-price").hide();
-//             $(".unlimited-month").hide();
-
-            
-//             // 不限月供
-//         }else if($this.hasClass("select-month")) {
-//             $(".unlimited-month").toggle();
-//             $(".unlimited-brand").hide();
-//             $(".unlimited-price").hide();
-//             $(".unlimited-firstpay").hide();
-
-//         }
-//     });
-// })
+        if($(".unlimited-brand").is(":visible") || $(".unlimited-price").is(":visible") || $(".unlimited-firstpay").is(":visible") || $(".unlimited-month").is(":visible") ){
+            // 遮罩层显示
+            $(".mark").show();
+        }else {
+            $(".mark").hide();
+        }
+    });
+})
