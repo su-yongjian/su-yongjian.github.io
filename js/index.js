240d9080a7f62d73241ee541f5e27552da95c27a
suyongjian <ada_yongjian@163.com>
2017-12-18 10:35:15 +0800

    删除

--- js/index.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $("#testQualificationBtn").on("click",function(){
-        window.location.href="../html/sonHTML/byStagesBuyCar.html";
-    });
-    $("#seeMoreCarBtn").on("click",function(){
-        window.location.href="../html/buyCar.html";
-    });
-})
