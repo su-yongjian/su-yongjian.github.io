6cd623be3c6c83d7a6625ecf0cf4bc042f82e2ad
苏勇坚 <721765355@qq.com>
2018-01-02 16:46:03 +0800

    提交新增

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(function(){
+    $("#testQualificationBtn").on("click",function(){
+        window.location.href="../html/testQualification.html";
+    });
+    $("#seeMoreCarBtn").on("click",function(){
+        window.location.href="../html/buyCar.html";
+    });
+})
